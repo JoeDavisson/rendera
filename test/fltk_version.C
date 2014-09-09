58ce1005a935c8ae62f2665c018a35e7f2dbf071
rubicks <rubicks00@gmail.com>
2014-09-09 12:05:55 -0400

    corrects test/fltk_version.C indentation

--- test/fltk_version.C
+++ test/fltk_version.C
@@ -1,4 +1,4 @@
-/* rendera/test/fltk_version.cc */
+/* rendera/test/fltk_version.C */
 
 #include <iostream>
 #include <cassert>
@@ -8,22 +8,22 @@
 int
 main( int, char** )
 {
-    std::cout
-        << std::dec
-        << std::endl
-        << __PRETTY_FUNCTION__
-        << std::endl
-        << "FL_MAJOR_VERSION == " << FL_MAJOR_VERSION
-        << std::endl
-        << "FL_MINOR_VERSION == " << FL_MINOR_VERSION
-        << std::endl
-        << "FL_PATCH_VERSION == " << FL_PATCH_VERSION
-        << std::endl
-        << "FL_VERSION == " << FL_VERSION
-        << std::endl
-        ;
-    assert( 1 <= FL_MAJOR_VERSION );
-    assert( 3 <= FL_MINOR_VERSION );
-    assert( 0 <= FL_PATCH_VERSION );
-    return 0 ;
+  std::cout
+    << std::dec
+    << std::endl
+    << __PRETTY_FUNCTION__
+    << std::endl
+    << "FL_MAJOR_VERSION == " << FL_MAJOR_VERSION
+    << std::endl
+    << "FL_MINOR_VERSION == " << FL_MINOR_VERSION
+    << std::endl
+    << "FL_PATCH_VERSION == " << FL_PATCH_VERSION
+    << std::endl
+    << "FL_VERSION == " << FL_VERSION
+    << std::endl
+    ;
+  assert( 1 <= FL_MAJOR_VERSION );
+  assert( 3 <= FL_MINOR_VERSION );
+  assert( 0 <= FL_PATCH_VERSION );
+  return 0 ;
 }
