37e7fdde62776c20b4980163bb64971f9102f75f
Mortis69 <joe_7@sbcglobal.net>
2014-09-28 15:40:44 -0500

    cleanups

--- Text.cxx
+++ Text.cxx
@@ -31,8 +31,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
 namespace
 {
-  Bitmap *temp;
-  int temp_allocated = 0;
+  Bitmap *temp = 0;
 }
 
 Text::Text()
@@ -53,11 +52,11 @@ void Text::push(View *view)
              (stroke->x2 - stroke->x1) + 1,
              (stroke->y2 - stroke->y1) + 1, 0);
 
-  int x, y;
+  // render text to image
   Bitmap *dest = Bitmap::main;
   Blend::set(Brush::main->blend);
-  int imgx = view->imgx;
-  int imgy = view->imgy;
+
+  int x, y;
 
   for(y = 0; y < temp->h; y++)
   {
@@ -65,16 +64,21 @@ void Text::push(View *view)
     {
       int c = temp->getpixel(x, y);
       int t = getv(c);
+
       if(t < 255)
-        dest->setpixel(imgx - temp->w / 2 + x, imgy - temp->h / 2 + y,
-                       Brush::main->color, scaleVal(Brush::main->trans, t));
+      {
+        dest->setpixel(view->imgx - temp->w / 2 + x,
+                       view->imgy - temp->h / 2 + y,
+                       Brush::main->color,
+                       scaleVal(Brush::main->trans, t));
+      }
     }
   }
 
-  if(temp_allocated)
+  if(temp)
   {
     delete temp;
-    temp_allocated = 0;
+    temp = 0;
   }
 
   Blend::set(Blend::TRANS);
@@ -94,27 +98,32 @@ void Text::release(View *)
 
 void Text::move(View *view)
 {
+  // write text string to FLTK's offscreen image
   if(!started)
   {
     started = 1;
     active = 1;
-    int tw = 0;
-    int th = 0;
+
     int face = Gui::getFontFace();
     int size = Gui::getFontSize();
-    fl_font(face, size);
     const char *string = Gui::getTextInput();
+
+    fl_font(face, size);
+
+    // measure size of image we need
+    int tw = 0, th = 0;
     fl_measure(string, tw, th, 1);
+
+    // draw text string to offscreen image
     Fl_Offscreen offscreen = fl_create_offscreen(tw, th);
 
-    if(temp_allocated)
+    if(temp)
     {
       delete temp;
-      temp_allocated = 0;
+      temp = 0;
     }
 
     temp = new Bitmap(tw, th);
-    temp_allocated = 1;
     temp->clear(makeRgb(255, 255, 255));
     Fl_RGB_Image *image = new Fl_RGB_Image((unsigned char *)temp->data, temp->w, temp->h, 4, 0);
 
@@ -123,17 +132,19 @@ void Text::move(View *view)
     fl_rectf(0, 0, tw, th);
     fl_color(FL_BLACK);
     fl_draw(string, 0, th - 1 - fl_descent());
+
     fl_read_image((unsigned char *)temp->data, 0, 0, tw, th, 255);
 
     fl_end_offscreen();
     fl_delete_offscreen(offscreen);
   }
 
+  // create preview map
   int x, y;
-  Map *map = Map::main;
   int imgx = view->imgx;
   int imgy = view->imgy;
 
+  Map *map = Map::main;
   map->clear(0);
 
   for(y = 0; y < temp->h; y++)
@@ -142,12 +153,14 @@ void Text::move(View *view)
     {
       int c = temp->getpixel(x, y);
       int t = getv(c);
+
       if(t < 128)
         map->setpixel(imgx - temp->w / 2 + x, imgy - temp->h / 2 + y, 255);
     }
   }
 
-  stroke->size(imgx - temp->w / 2, imgy - temp->h / 2, imgx + temp->w / 2, imgy + temp->h / 2);
+  stroke->size(imgx - temp->w / 2, imgy - temp->h / 2,
+               imgx + temp->w / 2, imgy + temp->h / 2);
   redraw(view);
 }
 
