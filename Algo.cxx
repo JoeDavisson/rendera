5c28368dfdb4c614cd1ddf070e9048e2387f29c7
rubicks <rubicks00@gmail.com>
2014-10-16 09:03:45 -0400

    cleaning up bresenham unit

--- Algo.cxx
+++ Algo.cxx
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cmath>
+#include <iostream>
 
 #include "Algo.H"
 /* #include "Rendera.H" */
@@ -9,11 +10,46 @@
 
 namespace
 {
+#if 0
+  std::ostream&
+  operator<<( std::ostream&os, point const&pt )
+  {
+    return os << "{ x:" << pt.x << ", y:" << pt.y << " }" ;
+  }
+
+  std::ostream&
+  operator<<( std::ostream&os, line const&ln )
+  {
+    return os << "{ a:" << ln.a << ", b:" << ln.b << " }" ;
+  }
+
+  template< typename T >
+  std::ostream&
+  operator<<( std::ostream&os, std::vector< T >const&vec )
+  {
+    typedef std::vector< T >const vec_type ;
+    typedef typename vec_type::const_iterator iterator ;
+    iterator it = vec.begin(), end = vec.end() ;
+    os << "[" ;
+    if( it != end ) os << " " << *it++ ;
+    while( it != end ) os << ", " << *it++ ;
+    return os << " ]" ;
+  }
+#endif
+
+
   template< typename T >
   inline T _sign( T const &v )
   {
     static T const zero( 0 );
     return ( zero > v ) ? T( -1 ) : T( 1 );
+  }
+
+  point
+  _point( int const&x, int const&y )
+  {
+    point ret = { x, y } ;
+    return ret ;
   }
 
   int
@@ -30,62 +66,87 @@ namespace
     return b.y - a.y ;
   }
 
+#if 0
   point
-  _point( int const&x, int const&y )
+  _transpose( point const&o )
   {
-    point ret = { x, y } ;
-    return ret ;
+    return _point( o.y, o.x );
   }
+#endif
 
-  line
-  _line( point const&a, point const&b )
+
+  std::vector< point >
+  _bresenham_floating( point const&a, point const&b )
   {
-    line ret = { a, b } ;
-    return ret ;
-  }
-}
+    std::vector< point >ret ;
 
+    int dx = _dx( a, b );
+    int dy = _dy( a, b );
 
-std::vector< point >
-Algo::bresenham( line const&o )
-{
-  std::vector< point >ret ;
-
-  int dx = _dx( o.a, o.b );
-  int dy = _dy( o.a, o.b );
-
-  if( 0 == dx ){
-    for( int y = o.a.y ; y != o.b.y ; y += _sign( dy ) ){
-      ret.push_back( _point( o.a.x, y ) );
+    if( 0 == dx ){
+      for( int x = a.x, y = a.y ; y != b.y ; y += _sign( dy ) ){
+        ret.push_back( _point( x, y ) );
+      }
+      ret.push_back( b );
+      return ret ;
     }
-    ret.push_back( o.b );
-    return ret ;
-  }
 
-  if( 0 == dy ){
-    for( int x = o.a.x ; x != o.b.x ; x += _sign( dx ) ){
-      ret.push_back( _point( x, o.a.y ) );
+    if( 0 == dy ){
+      for( int x = a.x, y = a.y ; x != b.x ; x += _sign( dx ) ){
+        ret.push_back( _point( x, y ) );
+      }
+      ret.push_back( b );
+      return ret ;
     }
-    ret.push_back( o.b );
-    return ret ;
+
+    double err = 0.0 ;
+    double derr = std::abs( double(dy) / double(dx) );
+
+    if( 1.0 < derr ){
+      /* iterate over y, shim x */
+      derr = 1 / derr ;
+      for( int x = a.x, y = a.y ; y != b.y ; y += _sign( dy ) ){
+        ret.push_back( _point( x, y ) );
+        err += derr ;
+        if( 0.5 > err ) continue ;
+        x += _sign( dx );
+        err -= 1.0 ;
+      }
+      ret.push_back( b );
+      return ret ;
+    }else{
+      /* iterate over x, shim y */
+      for( int x = a.x, y = a.y ; x != b.x ; x += _sign( dx ) ){
+        ret.push_back( _point( x, y ) );
+        err += derr ;
+        if( 0.5 > err ) continue ;
+        y += _sign( dy );
+        err -= 1.0 ;
+      }
+      ret.push_back( b );
+      return ret ;
+    }
   }
 
-  double err = 0.0 ;
-  double derr = std::abs( double(dy) / double(dx) );
-  for( int x = o.a.x, y = o.a.y ; x != o.b.x ; x += _sign( dx ) ){
-    ret.push_back( _point( x, y ) );
-    err += derr ;
-    if( 0.5 > err ) continue ;
-    y += _sign( dy );
-    err -= 1.0 ;
+
+  std::vector< point >
+  _bresenham( point const&a, point const&b )
+  {
+    return _bresenham_floating( a, b );
   }
-  ret.push_back( _point( o.b.x, o.b.y ) );
-  return ret ;
 }
 
 
 std::vector< point >
 Algo::bresenham( point const&a, point const&b )
 {
-  return bresenham( _line( a, b ) );
+  return _bresenham( a, b );
 }
+
+std::vector< point >
+Algo::bresenham( line const&ln )
+{
+  return bresenham( ln.a, ln.b );
+}
+
+
--- test/bresenham.C
+++ test/bresenham.C
@@ -12,29 +12,18 @@
 
 namespace
 {
-  std::ostream&
-  operator<<( std::ostream&os, point const&pt )
+  point
+  _point( int const&x, int const&y )
   {
-    return os << "{ x:" << pt.x << ", y:" << pt.y << " }" ;
+    point ret = { x, y } ;
+    return ret ;
   }
 
-  std::ostream&
-  operator<<( std::ostream&os, line const&ln )
+  line
+  _line( point const&a, point const&b )
   {
-    return os << "{ a:" << ln.a << ", b:" << ln.b << " }" ;
-  }
-
-  template< typename T >
-  std::ostream&
-  operator<<( std::ostream&os, std::vector< T >vec )
-  {
-    typedef std::vector< T >vec_type ;
-    typedef typename vec_type::iterator iterator ;
-    iterator it = vec.begin(), end = vec.end() ;
-    os << "[" ;
-    if( it != end ) os << " " << *it++ ;
-    while( it != end ) os << ", " << *it++ ;
-    return os << " ]" ;
+    line ret = { a, b } ;
+    return ret ;
   }
 }
 
@@ -42,59 +31,47 @@ namespace
 int
 main( int, char** )
 {
-  point const foo = { 2, 5 } ;
-  point const bar = { 5, 7 } ;
-  line const foobar = { foo, bar } ;
+  {
+    point const a = _point(0,0);
+    point const b = _point(5,7);
+    std::vector< point >const ln = Algo::bresenham( _line( a, b ) );
+    assert( 8 == ln.size() );
+    assert( _point(0,0) == ln[0] );
+    assert( _point(1,1) == ln[1] );
+    assert( _point(1,2) == ln[2] );
+    assert( _point(2,3) == ln[3] );
+    assert( _point(3,4) == ln[4] );
+    assert( _point(4,5) == ln[5] );
+    assert( _point(4,6) == ln[6] );
+    assert( _point(5,7) == ln[7] );
+  }
 
-  std::cout
-    << std::dec
-    << std::endl
-    << __PRETTY_FUNCTION__
-    << std::endl
-    << "foo == " << foo
-    << std::endl
-    << "bar == " << bar
-    << std::endl
-    << "foobar == " << foobar
-    << std::endl
-    ;
+  {
+    std::vector< point >const ln = Algo::bresenham( _point(0,0), _point(0,0) );
+    assert( 1 == ln.size() );
+    assert( _point(0,0) == ln[0] );
+  }
 
-  std::cout
-    << std::endl
-    << "std::vector< int >( 0 ) == " << std::vector< int >( 0 )
-    << std::endl
-    << "std::vector< int >( 1 ) == " << std::vector< int >( 1 )
-    << std::endl
-    << "std::vector< int >( 2 ) == " << std::vector< int >( 2 )
-    << std::endl
-    ;
+  {
+    std::vector< point >const ln = Algo::bresenham( _point(0,0), _point(0,1) );
+    assert( 2 == ln.size() );
+    assert( _point(0,0) == ln[0] );
+    assert( _point(0,1) == ln[1] );
+  }
 
-  std::cout
-    << std::endl
-    << "Algo::bresenham( foobar ) == " << Algo::bresenham( foobar )
-    << std::endl
-    ;
+  {
+    std::vector< point >const ln = Algo::bresenham( _point(0,0), _point(1,0) );
+    assert( 2 == ln.size() );
+    assert( _point(0,0) == ln[0] );
+    assert( _point(1,0) == ln[1] );
+  }
 
-  point const p00 = { 0, 0 } ;
-  point const p01 = { 0, 1 } ;
-  point const p10 = { 1, 0 } ;
-  point const p11 = { 1, 1 } ;
-
-  std::cout
-    << std::endl
-    << "Algo::bresenham( " << p00 << ", " << p00 << " ) == "
-    << Algo::bresenham( p00, p00 )
-    << std::endl
-    << "Algo::bresenham( " << p00 << ", " << p01 << " ) == "
-    << Algo::bresenham( p00, p01 )
-    << std::endl
-    << "Algo::bresenham( " << p00 << ", " << p10 << " ) == "
-    << Algo::bresenham( p00, p10 )
-    << std::endl
-    << "Algo::bresenham( " << p00 << ", " << p11 << " ) == "
-    << Algo::bresenham( p00, p11 )
-    << std::endl
-    ;
+  {
+    std::vector< point >const ln = Algo::bresenham( _point(0,0), _point(1,1) );
+    assert( 2 == ln.size() );
+    assert( _point(0,0) == ln[0] );
+    assert( _point(1,1) == ln[1] );
+  }
 
   return EXIT_SUCCESS ;
 }
