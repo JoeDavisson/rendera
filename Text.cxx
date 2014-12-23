1aa43d33905aeb15471debe82936cda70c6fb1ad
Mortis69 <joe_7@sbcglobal.net>
2014-12-23 13:02:52 -0600

    removed unused variable

--- Text.cxx
+++ Text.cxx
@@ -142,8 +142,8 @@ void Text::move(View *view)
 
     temp = new Bitmap(tw, th);
     temp->clear(makeRgb(255, 255, 255));
-    Fl_RGB_Image *image = new Fl_RGB_Image((unsigned char *)temp->data,
-                                           temp->w, temp->h, 4, 0);
+    //Fl_RGB_Image *image = new Fl_RGB_Image((unsigned char *)temp->data,
+    //                                       temp->w, temp->h, 4, 0);
 
     fl_begin_offscreen(offscreen);
     fl_color(FL_WHITE);
