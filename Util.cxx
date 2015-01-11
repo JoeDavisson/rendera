ca3b34a44cf8661b809a750d033a1270d5efbf75
rubicks <rubicks00@gmail.com>
2015-01-10 20:36:07 -0500

    adds Util header and source

---
+++ Util.cxx
@@ -0,0 +1,35 @@
+/* rendera/Util.cxx */
+
+#if HAVE_CONFIG_H
+#  include "config.h"
+#else
+#  error "missing config.h"
+#endif
+
+#include <limits.h>
+#include <unistd.h>
+
+#include "Util.H"
+
+namespace
+{
+  std::string
+  _readlink( std::string const&path )
+  {
+    char buf[ PATH_MAX ] = { 0 };
+    ssize_t count = readlink( path.c_str(), buf, PATH_MAX );
+    return std::string( buf, std::max( count, ssize_t( 0 ) ) );
+  }
+}
+
+std::string
+Util::readlink( std::string const&path )
+{
+  return _readlink( path );
+}
+
+std::string
+Util::parent_path( std::string const&path )
+{
+  return path.substr( 0, path.find_last_of( "/\\" ) );
+}
