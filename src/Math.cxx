6ae56263472433b5eb53b731cb023c25a67bd47a
rubicks <rubicks00@gmail.com>
2015-02-11 12:42:39 -0500

    cleans Math header and source, adds numeric tests for Math::abs

--- src/Math.cxx
+++ src/Math.cxx
@@ -18,6 +18,8 @@ along with Rendera; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 */
 
+#include <cmath>
+
 #include "Math.H"
 
 // fft routines adapted from those found here:
