8c7bd96372a1500a31407dbebeb78574f4c35abc
Mortis69 <joe_7@sbcglobal.net>
2015-02-17 08:58:28 -0600

    disabled descreen filter (doesn't work yet)

--- src/Gui.cxx
+++ src/Gui.cxx
@@ -275,8 +275,8 @@ void Gui::init()
     (Fl_Callback *)FX::convolutionMatrix, 0, 0);
   menubar->add("Effects/Artistic...", 0,
     (Fl_Callback *)FX::artistic, 0, 0);
-  menubar->add("Effects/Descreen...", 0,
-    (Fl_Callback *)FX::descreen, 0, 0);
+//  menubar->add("Effects/Descreen...", 0,
+//    (Fl_Callback *)FX::descreen, 0, 0);
   menubar->add("&Help/&About...", 0,
     (Fl_Callback *)Dialog::about, 0, 0);
 
