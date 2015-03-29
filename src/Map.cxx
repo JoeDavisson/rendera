8dd6c6d27e098249be77687f63eeed5063703ede
Mortis69 <joe_7@sbcglobal.net>
2015-03-29 09:43:07 -0500

    wrong header

--- src/Map.cxx
+++ src/Map.cxx
@@ -20,7 +20,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
 #include <algorithm>
 #include <cstdlib>
-#include <unistd.h>
+#include <stdint.h>
 
 #include "Map.H"
 #include "Math.H"
