84ea4b857aa3cc3f6cfeb723f9ef8b8b8e747ac6
JoeDavisson <joe63074@gmail.com>
2025-11-27 15:53:32 -0600

    improvements

--- src/FX/CubePlot.cxx
+++ src/FX/CubePlot.cxx
@@ -31,59 +31,25 @@ namespace
   }
 }
 
-void CubePlot::applyAll()
+void CubePlot::apply(const bool use_palette)
 {
-  Bitmap *bmp = Project::bmp;
-  std::vector<float> histogram(16777216, 0);
-
+  Palette *pal = Project::palette;
+  Bitmap *src = Project::bmp;
   Project::newImage(512, 512);
+  Bitmap *dest = Project::bmp;
   Gui::images->addFile("cube_plot");
   Project::undo->reset();
+  Progress::show(256);
+  std::vector<char> histogram(16777216, 0);
 
-  for (int j = bmp->ct; j <= bmp->cb; j++)
+  for (int j = src->ct; j <= src->cb; j++)
   {
-    for (int i = bmp->cl; i <= bmp->cr; i++)
+    for (int i = src->cl; i <= src->cr; i++)
     {
-      rgba_type rgba = getRgba(bmp->getpixel(i, j));
+      rgba_type rgba = getRgba(src->getpixel(i, j));
       histogram[makeRgb24(rgba.r, rgba.g, rgba.b)] = 1;
     }
   }
-
-  Progress::show(256);
-
-  for (int b = 0; b < 256; b++)
-  {
-    for (int g = 0; g < 256; g++)
-    {
-      for (int r = 0; r < 256; r++)
-      {
-        int x = (r - g) * std::cos(30 * (M_PI / 180));
-        int y = (r + g) * std::sin(30 * (M_PI / 180)) - b;
-        int c = makeRgb(r, g, b);
-
-        if (((r & 7) == 0) && ((g & 7) == 0) && ((b & 7) == 0))
-          Project::bmp->setpixel(256 + x, 256 + y, c, 224);
-
-        if (histogram[c & 0xffffff] == 1)
-        {
-          Project::bmp->setpixel(256 + x, 256 + y, c, 96);
-        }
-      }
-    }
-
-    Progress::update(b);
-  }
-
-  Progress::hide();
-}
-
-void CubePlot::applyPalette()
-{
-  Palette *pal = Project::palette;
-
-  Project::newImage(512, 512);
-  Gui::images->addFile("cube_plot");
-  Project::undo->reset();
 
   for (int b = 0; b < 256; b++)
   {
@@ -94,20 +60,49 @@ void CubePlot::applyPalette()
         int x = (r - g) * std::cos(30 * (M_PI / 180));
         int y = (r + g) * std::sin(30 * (M_PI / 180)) - b;
         int c1 = makeRgb(r, g, b);
-        int c2 = pal->data[pal->lookup(c1)];
 
-        if (((r & 7) == 0) && ((g & 7) == 0) && ((b & 7) == 0))
-          Project::bmp->setpixel(256 + x, 256 + y, c1, 224);
+        int tr = (r == 0) || (r == 255) ? 1 : 0;
+        int tg = (g == 0) || (g == 255) ? 1 : 0;
+        int tb = (b == 0) || (b == 255) ? 1 : 0;
 
-        int d = diff24(c1, c2);
-
-        if (d < 8)
+        if (tr + tg + tb == 2)
         {
-          Project::bmp->setpixel(256 + x, 256 + y, c2, 96);
+          dest->setpixel(256 + x, 256 + y, c1, 128);
+        }
+
+        if (((r & 15) == 0) && ((g & 15) == 0) && ((b & 15) == 0))
+        {
+          dest->setpixel(256 + x, 256 + y, c1, 208);
+          dest->rect(256 + x - 1, 256 + y - 1,
+                     256 + x + 1, 256 + y + 1, c1, 240);
+        }
+
+        if (use_palette == true)
+        {
+          int c2 = pal->data[pal->lookup(c1)];
+          int d = diff24(c1, c2);
+
+          if (d < 8)
+          {
+            dest->setpixel(256 + x, 256 + y, c2, 96);
+          }
+        }
+          else
+        {
+          if (histogram[c1 & 0xffffff] > 0)
+          {
+            dest->setpixel(256 + x, 256 + y, c1, 96);
+            dest->rect(256 + x - 1, 256 + y - 1,
+                       256 + x + 1, 256 + y + 1, c1, 240);
+          }
         }
       }
     }
+
+    Progress::update(b);
   }
+
+  Progress::hide();
 }
 
 void CubePlot::close()
@@ -115,9 +110,9 @@ void CubePlot::close()
   Items::dialog->hide();
 
   if (Items::pal_colors->value())
-    applyPalette();
+    apply(true);
   else
-    applyAll();
+    apply(false);
 }
 
 void CubePlot::quit()
@@ -137,7 +132,8 @@ void CubePlot::init()
 
   Items::dialog = new DialogWindow(400, 0, "Cube Plot");
 
-  Items::pal_colors = new CheckBox(Items::dialog, 0, y1, 16, 16, "Palette Colors", 0);
+  Items::pal_colors = new CheckBox(Items::dialog, 0, y1, 16, 16,
+                                   "Palette Colors", 0);
   y1 += 16 + 16;
   Items::pal_colors->center();
 
